5126574b721b0ccdf83a97103f4b203a1830e181
Rafael Sousa <rafael9081@gmail.com>
2025-07-27 21:25:30 +0000

    File changes

---
+++ src/lib/firebase/config.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  "projectId": "new-prototype-totdc",
+  "appId": "1:13417956393:web:7962e44176d073bfb1c838",
+  "storageBucket": "new-prototype-totdc.firebasestorage.app",
+  "apiKey": "AIzaSyBf7_6mEuT6Fq3Pzd9n74ZVxV2fpEtlNuk",
+  "authDomain": "new-prototype-totdc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "13417956393"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+
+export { app, db };
