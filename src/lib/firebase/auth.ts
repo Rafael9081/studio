012070beca1af789e1b89f81307489e9f002948b
Rafael Sousa <rafael9081@gmail.com>
2025-07-28 14:08:54 +0000

    nao foi ainda

--- src/lib/firebase/auth.ts
+++ src/lib/firebase/auth.ts
@@ -1,6 +1,3 @@
-
-'use client';
-
 import { auth } from './config';
 import { 
     signInWithEmailAndPassword,
--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "new-prototype-totdc",
-  "appId": "1:13417956393:web:7962e44176d073bfb1c838",
-  "storageBucket": "new-prototype-totdc.appspot.com",
   "apiKey": "AIzaSyA8YhC-pC4c-9fW3wQjSg3qZ7oY4xR-dE4",
   "authDomain": "new-prototype-totdc.firebaseapp.com",
-  "messagingSenderId": "13417956393"
+  "projectId": "new-prototype-totdc",
+  "storageBucket": "new-prototype-totdc.appspot.com",
+  "messagingSenderId": "13417956393",
+  "appId": "1:13417956393:web:7962e44176d073bfb1c838"
 };
 
 // Initialize Firebase
